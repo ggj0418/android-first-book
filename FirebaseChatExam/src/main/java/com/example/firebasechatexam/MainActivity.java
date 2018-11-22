31df42e411ea0d003769766ffee0f5008292fa9a
ggj0418 <44552228+ggj0418@users.noreply.github.com>
2018-11-23 00:39:01 +0900

    Updata MainActivity.java
    MainActivity를 폰에서 가동했을 때 주소록하고 연동해서 초대메시지를 보내는 기능을 구현하려고 했으나 실패함. 가이드라인 따라서 했는데 무엇이 문제인지 파악하지 못함. 추후 개선할 필요가 있음

--- FirebaseChatExam/src/main/java/com/example/firebasechatexam/MainActivity.java
+++ FirebaseChatExam/src/main/java/com/example/firebasechatexam/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.firebasechatexam;
+package com.example.ggj04.chatprogramwithfb;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -17,9 +19,9 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.crashlytics.android.Crashlytics;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.gms.appinvite.AppInvite;
@@ -43,59 +45,81 @@ import java.util.Map;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
-
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    private static final int REQUEST_INVITE = 1000;
+    private FirebaseRemoteConfig mFirebaseRemoteConfig;
+
+    public static final int REQUEST_INVITE = 1000;
+    private FirebaseRecyclerAdapter<ChatMessage, MessageViewHolder> mFirebaseAdapter;
 
     public static final String MESSAGES_CHILD = "messages";
     private DatabaseReference mFirebaseDatabaseReference;
     private EditText mMessageEditText;
 
-    private FirebaseRecyclerAdapter<ChatMessage, MessageViewHolder> mFirebaseAdapter;
-    private RecyclerView mMessageRecyclerView;
-
-    // Firebase 인스턴스 변수
     private FirebaseAuth mFirebaseAuth;
     private FirebaseUser mFirebaseUser;
-    private FirebaseRemoteConfig mFirebaseRemoteConfig;
 
-    // Google
-    private GoogleApiClient mGoogleApiClient;
-
-    // 사용자 이름과 사진
     private String mUsername;
     private String mPhotoUrl;
 
+    private GoogleApiClient mGoogleApiClient;
+
+    @NonNull
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        Toast.makeText(this, "Google Play Services error.", Toast.LENGTH_SHORT).show();
+
     }
 
     public static class MessageViewHolder extends RecyclerView.ViewHolder {
+
+        TextView nameTextView;
         TextView messageTextView;
         ImageView messageImageView;
-        TextView messengerTextView;
-        CircleImageView messengerImageView;
+        CircleImageView photoImageView;
 
-        public MessageViewHolder(View v) {
-            super(v);
-            messageTextView = itemView.findViewById(R.id.messageTextView);
+        public MessageViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            nameTextView = itemView.findViewById(R.id.nameTextView);
             messageImageView = itemView.findViewById(R.id.messageImageView);
-            messengerTextView = itemView.findViewById(R.id.messengerTextView);
-            messengerImageView = itemView.findViewById(R.id.messengerImageView);
+            messageTextView = itemView.findViewById(R.id.messageTextView);
+            photoImageView = itemView.findViewById(R.id.photoImageView);
         }
     }
+
+    private RecyclerView mMessageRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mFirebaseDatabaseReference = FirebaseDatabase.getInstance().getReference();
+        mMessageEditText = findViewById(R.id.message_edit);
+
+        mMessageRecyclerView = findViewById(R.id.message_recycler_view);
+
+        findViewById(R.id.send_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ChatMessage chatMessage = new ChatMessage(mMessageEditText.getText().toString(),
+                        mUsername, mPhotoUrl, null);
+                mFirebaseDatabaseReference.child(MESSAGES_CHILD)
+                        .push()
+                        .setValue(chatMessage);
+                mMessageEditText.setText("");
+            }
+        });
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API)
+                .addApi(AppInvite.API)
+                .build();
+
         mFirebaseAuth = FirebaseAuth.getInstance();
         mFirebaseUser = mFirebaseAuth.getCurrentUser();
         if (mFirebaseUser == null) {
-            // 인증이 안 되었다면 인증 화면으로 이동
             startActivity(new Intent(this, SignInActivity.class));
             finish();
             return;
@@ -106,90 +130,50 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
             }
         }
 
-        mMessageRecyclerView = findViewById(R.id.message_recycler_view);
-
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
-                .addApi(Auth.GOOGLE_SIGN_IN_API)
-                .addApi(AppInvite.API)
+        Query query = mFirebaseDatabaseReference.child(MESSAGES_CHILD);
+        FirebaseRecyclerOptions<ChatMessage> options = new FirebaseRecyclerOptions.Builder<ChatMessage>()
+                .setQuery(query, ChatMessage.class)
                 .build();
 
-
-        // Firebase 리얼타임 데이터 베이스 초기화
-        mFirebaseDatabaseReference = FirebaseDatabase.getInstance().getReference();
-
-
-        mMessageEditText = findViewById(R.id.message_edit);
-        // 보내기 버튼
-        findViewById(R.id.send_button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ChatMessage chatMessage = new ChatMessage(mMessageEditText.getText().toString(),
-                        mUsername,
-                        mPhotoUrl,
-                        null);
-                mFirebaseDatabaseReference.child(MESSAGES_CHILD)
-                        .push()
-                        .setValue(chatMessage);
-                mMessageEditText.setText("");
-            }
-        });
-
-        // 쿼리 수행 위치
-        Query query = mFirebaseDatabaseReference.child(MESSAGES_CHILD);
-
-        // 옵션
-        FirebaseRecyclerOptions<ChatMessage> options =
-                new FirebaseRecyclerOptions.Builder<ChatMessage>()
-                        .setQuery(query, ChatMessage.class)
-                        .build();
-
-        // 어댑터
         mFirebaseAdapter = new FirebaseRecyclerAdapter<ChatMessage, MessageViewHolder>(options) {
             @Override
             protected void onBindViewHolder(MessageViewHolder holder, int position, ChatMessage model) {
                 holder.messageTextView.setText(model.getText());
-                holder.messengerTextView.setText(model.getName());
+                holder.nameTextView.setText(model.getName());
                 if (model.getPhotoUrl() == null) {
-                    holder.messengerImageView.setImageDrawable(ContextCompat.getDrawable(MainActivity.this,
+                    holder.photoImageView.setImageDrawable(ContextCompat.getDrawable(MainActivity.this,
                             R.drawable.ic_account_circle_black_24dp));
                 } else {
                     Glide.with(MainActivity.this)
                             .load(model.getPhotoUrl())
-                            .into(holder.messengerImageView);
+                            .into(holder.photoImageView);
                 }
             }
 
+            @NonNull
             @Override
-            public MessageViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            public MessageViewHolder onCreateViewHolder(ViewGroup parent, int i) {
                 View view = LayoutInflater.from(parent.getContext())
                         .inflate(R.layout.item_message, parent, false);
                 return new MessageViewHolder(view);
             }
         };
 
-        // 리사이클러뷰에 레이아웃 매니저와 어댑터 설정
         mMessageRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         mMessageRecyclerView.setAdapter(mFirebaseAdapter);
 
-        // Firebase Remote Config 초기화
         mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
-
-        // Firebase Remote Config 설정
         FirebaseRemoteConfigSettings firebaseRemoteConfigSettings =
                 new FirebaseRemoteConfigSettings.Builder()
-                        .setDeveloperModeEnabled(true)
-                        .build();
+                .setDeveloperModeEnabled(true)
+                .build();
 
-        // 인터넷 연결이 안 되었을 때 기본 값 정의
         Map<String, Object> defaultConfigMap = new HashMap<>();
         defaultConfigMap.put("message_length", 10L);
-
-        // 설정과 기본 값 설정
+        
         mFirebaseRemoteConfig.setConfigSettings(firebaseRemoteConfigSettings);
         mFirebaseRemoteConfig.setDefaults(defaultConfigMap);
-
-        // 원격 구성 가져오기
+    
         fetchConfig();
 
         // 새로운 글이 추가되면 제일 하단으로 포지션 이동
@@ -225,17 +209,45 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
         });
     }
 
+    private void fetchConfig() {
+        long cacheExpiration = 3600;
+        if(mFirebaseRemoteConfig.getInfo().getConfigSettings().isDeveloperModeEnabled()) {
+            cacheExpiration = 0;
+        }
+
+        mFirebaseRemoteConfig.fetch(cacheExpiration)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        mFirebaseRemoteConfig.activateFetched();
+                        applyRetrievedLengthLimit();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.w(TAG, "Error fetching config" + e.getMessage());
+                applyRetrievedLengthLimit();
+            }
+        });
+    }
+
+    private void applyRetrievedLengthLimit() {
+        Long messageLength = mFirebaseRemoteConfig.getLong("message_length");
+        mMessageEditText.setFilters(new InputFilter[]{
+                new InputFilter.LengthFilter(messageLength.intValue())
+        });
+        Log.d(TAG, "applyRetrievedLengthLimit: " + messageLength);
+    }
+
     @Override
     protected void onStart() {
         super.onStart();
-        // FirebaseRecyclerAdapter 실시간 쿼리 시작
         mFirebaseAdapter.startListening();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        // FirebaseRecyclerAdapter 실시간 쿼리 중지
         mFirebaseAdapter.stopListening();
     }
 
@@ -247,7 +259,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
+        switch(item.getItemId()) {
             case R.id.sign_out_menu:
                 mFirebaseAuth.signOut();
                 Auth.GoogleSignInApi.signOut(mGoogleApiClient);
@@ -256,78 +268,33 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
                 finish();
                 return true;
             case R.id.invitation_menu:
-                sendInvitation();
+                onInviteClicked();
                 return true;
             case R.id.crash_menu:
-                throw new RuntimeException("치명적인 버그!!");
+                Crashlytics.getInstance().crash();
+                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
-
-    // 원격 구성 가져오기
-    public void fetchConfig() {
-        long cacheExpiration = 3600; // 1시간
-        // 개발자 모드라면 0초로 하기
-        if (mFirebaseRemoteConfig.getInfo().getConfigSettings()
-                .isDeveloperModeEnabled()) {
-            cacheExpiration = 0;
-        }
-        mFirebaseRemoteConfig.fetch(cacheExpiration)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        // 원격 구성 가져오기 성공
-                        mFirebaseRemoteConfig.activateFetched();
-                        applyRetrievedLengthLimit();
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        // 원격 구성 가져오기 실패
-                        Log.w(TAG, "Error fetching config: " +
-                                e.getMessage());
-                        applyRetrievedLengthLimit();
-                    }
-                });
-    }
-
-
-    /**
-     * 서버에서 가져 오거나 캐시된 값을 가져 옴
-     */
-    private void applyRetrievedLengthLimit() {
-        Long messageLength =
-                mFirebaseRemoteConfig.getLong("message_length");
-        mMessageEditText.setFilters(new InputFilter[]{new
-                InputFilter.LengthFilter(messageLength.intValue())});
-        Log.d(TAG, "메시지 길이 : " + messageLength);
-    }
-
-    private void sendInvitation() {
-        Intent intent = new AppInviteInvitation.IntentBuilder("초대 제목")
-                .setMessage("채팅앱에 초대합니다")
-                .setCallToActionText("채팅에 참여하기")
+    
+    private void onInviteClicked() {                // 다른 사람 초대 
+        Intent intent = new AppInviteInvitation.IntentBuilder("Invite Title")
+                .setMessage("Welcome to Chat Program")
+                .setDeepLink(Uri.parse("https://play.google.com/store/apps/details?id=com.example.ggj04.chatprogramwithfb"))
+                .setCallToActionText("JOIN?")
                 .build();
         startActivityForResult(intent, REQUEST_INVITE);
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        Log.d(TAG, "onActivityResult: requestCode=" + requestCode + ", resultCode=" + resultCode);
-
-        if (requestCode == REQUEST_INVITE) {
-            if (resultCode == RESULT_OK) {
-                // 얼마나 많은 초대를 보냈는지
+        if(requestCode == REQUEST_INVITE) {
+            if(resultCode == RESULT_OK) {
                 String[] ids = AppInviteInvitation.getInvitationIds(resultCode, data);
-                Log.d(TAG, "Invitations sent: " + ids.length);
             } else {
-                // 초대가 취소되거나 실패함
-                Log.d(TAG, "Failed to send invitation.");
             }
         }
     }
-
 }
