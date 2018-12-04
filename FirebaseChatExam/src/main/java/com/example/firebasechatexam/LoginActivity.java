7d8f5c3619f8bfe250fbda2cfd8f2e161df1847c
ggj0418 <44552228+ggj0418@users.noreply.github.com>
2018-12-04 22:43:27 +0900

    Update LoginActivity.java

--- FirebaseChatExam/src/main/java/com/example/firebasechatexam/LoginActivity.java
+++ FirebaseChatExam/src/main/java/com/example/firebasechatexam/LoginActivity.java
@@ -30,7 +30,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         firebaseAuth = FirebaseAuth.getInstance();
-        firebaseAuth.signOut();
+        firebaseAuth.signOut();                     // 앱 실행 시마다 새로 로그아웃을 해서 항상 로그인 화면이 나오도록 설정
 
         id = (EditText) findViewById(R.id.loginActivity_edittext_id);
         password = (EditText) findViewById(R.id.loginActivity_edittext_password);
@@ -38,14 +38,14 @@ public class LoginActivity extends AppCompatActivity {
         login = (Button)findViewById(R.id.loginActivity_button_login);
         signup = (Button)findViewById(R.id.loginActivity_button_signin);
 
-        login.setOnClickListener(new View.OnClickListener() {
+        login.setOnClickListener(new View.OnClickListener() {   // 로그인 버튼 클릭 시 loginEvent 함수를 실행시키는 이벤트 설정
             @Override
             public void onClick(View view) {
                 loginEvent();
             }
         });
 
-        signup.setOnClickListener(new View.OnClickListener() {
+        signup.setOnClickListener(new View.OnClickListener() {  // 회원가입 버튼 클릭시 SignupActivity로 넘어가는 이벤트 설정
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(LoginActivity.this,SignupActivity.class));
@@ -57,12 +57,12 @@ public class LoginActivity extends AppCompatActivity {
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
                 if(user != null){
-                    //로그인
+                    // 현재 로그인 신청한 사용자가 데이터베이스에 등록되어있는 사용자라면 MainActivity로 넘어감
                     Intent intent = new Intent(LoginActivity.this,MainActivity.class);
                     startActivity(intent);
                     finish();
                 }else{
-                    //로그아웃
+                    // 로그인 신청한 사용자의 정보가 없으면 firebaseAuth.signOut(); 으로 돌아가서 다시 로그인화면을 띄움
                 }
 
             }
@@ -77,7 +77,7 @@ public class LoginActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
 
                         if (!task.isSuccessful()) {
-                            //로그인 실패한부분
+                            //로그인에 실패하면 실해했다고 메시지를 띄워줌
                             Toast.makeText(LoginActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                         }
 
@@ -90,7 +90,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        firebaseAuth.addAuthStateListener(authStateListener);
+        firebaseAuth.addAuthStateListener(authStateListener);   
     }
 
     @Override
