90be9c97ea7df5154ea7e7e0ea30dbf9569c3cc3
ggj0418 <44552228+ggj0418@users.noreply.github.com>
2018-12-04 23:16:30 +0900

    Update PeopleFragment.java

--- FirebaseChatExam/src/main/java/com/example/firebasechatexam/fragment/PeopleFragment.java
+++ FirebaseChatExam/src/main/java/com/example/firebasechatexam/fragment/PeopleFragment.java
@@ -24,6 +24,7 @@ public class PeopleFragment extends Fragment {
 
     @Nullable
     @Override
+    // 데이터베이스에 있는 학과들을 리스트뷰로 보여주는 View 설정
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_people, container, false);
         RecyclerView recyclerView = (RecyclerView)view.findViewById(R.id.peoplefragment_recyclerview);
@@ -33,16 +34,19 @@ public class PeopleFragment extends Fragment {
         return view;
     }
 
+    // 데이터베이스에서 불러온 학과정보를 View로 담아주는 Adapter 설정
     class PeopleFragmentRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
         List<UserModel> userModels;
 
+        // 리스트뷰의 요소인 item_friend를 읽어와서 View 생성
         @Override
         public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_friend,parent,false);
             return new CustomViewHolder(view);
         }
 
+        // ViewHolder에 학과이름과 사진을 설정
         @Override
         public void onBindViewHolder(RecyclerView.ViewHolder holder, final int position) {
             try {
@@ -53,12 +57,12 @@ public class PeopleFragment extends Fragment {
                         .apply(new RequestOptions().circleCrop())
                         .into(((CustomViewHolder) holder).imageView);
 
+                // 리스트뷰에서 각각의 리스트를 클릭했을 때 MessageActivity로 넘어가는 이벤트 생성
                 holder.itemView.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
                         Intent intent = new Intent(view.getContext(), MessageActivity.class);
-                        intent.putExtra("departmentName", userModels.get(position).userName);
-                        // intent.putExtra("currentUserID", userID);
+                        intent.putExtra("departmentName", userModels.get(position).userName);                      
                         ActivityOptions activityOptions = null;
                         activityOptions = ActivityOptions.makeCustomAnimation(view.getContext(), R.anim.fromright,R.anim.toleft);
                         startActivity(intent,activityOptions.toBundle());
@@ -74,6 +78,7 @@ public class PeopleFragment extends Fragment {
             return userModels.size();
         }
 
+        // 리스트뷰 요소와 xml 
         private class CustomViewHolder extends RecyclerView.ViewHolder {
             public ImageView imageView;
             public TextView textView;
